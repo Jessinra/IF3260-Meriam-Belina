556e2542c0f8d6124e51a17528737b5b8a90899b
Tony <buddy90_lost@yahoo.co.id>
2019-01-27 22:02:46 +0700

    woi

---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include "master.hpp"
+#include "Object.hpp"
+
+using namespace std;
+
+class Runner : public Master{
+protected:
+    Object pesawat, meriam;
+public:
+    Runner(int h=700, int w = 1000) : Master(h, w) {
+        pesawat = Object(0, 0, "object_plane.txt");
+        // meriam = Object(0, 0, "meriam.txt");
+    }
+    void start() {
+        clearWindow();
+        drawObject(pesawat);
+        // while(true){
+        //     clearWindow();
+        //     pesawat.moveLeft(framejump);
+        //     drawObject(pesawat);
+        //     // cout<<pesawat.getRefPos().getX()<<" "<<pesawat.getRefPos().getY()<<endl;
+        //     if(pesawat.getRefPos().getX() <= 0)
+        //         pesawat.setPos(Pixel(xend, 0));
+        //     usleep(200);
+        // }
+    }
+};
+
+int main(){
+    Runner run;
+    run.start();
+    return 0;
+}
